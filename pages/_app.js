f0dd622de808851d38b1c5e7c296f8f975131696
James Murty <james@murty.co>
2021-08-25 16:02:02 +1000

    Tweak following Vercel Next.js Sentry integration example
    https://github.com/vercel/next.js/tree/canary/examples/with-sentry

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, err }) {
+  return <Component {...pageProps} err={err} />;
 }
 
 export default MyApp
